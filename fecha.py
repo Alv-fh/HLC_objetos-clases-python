b9926846250d3dd60f9a11552d260e67eb89a114
Álvaro <109484163+Alv-fh@users.noreply.github.com>
2024-02-12 14:52:39 +0100

    Create fecha.py

---
+++ fecha.py
@@ -0,0 +1,23 @@
+class Fecha():
+    def __init__(self):
+        self._dia=0
+        self._mes=0
+        self._año=0
+        self._comprobar
+      
+    def modificar_fecha(self, dia):
+        Fecha._comprobar_dia(dia)
+        Fecha._comprobar_mes(mes)
+      
+    def modificar_dia(self, dia)
+        if dia > 1 and dia < 31:
+          return True
+      
+    def modificar_mes(self, mes)
+        if mes > 1 and mes < 12:
+          return True
+
+    def modificar_año(self, año)
+        
+
+  
